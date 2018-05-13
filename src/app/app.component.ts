08d9b15c89185a49a6897b1735a18b1f53f5caf6
Wesley Smith <wesleysmith1@mail.weber.edu>
2018-05-12 22:05:11 -0600

    changed title for matina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Marina is going to make the css for this site';
 }
